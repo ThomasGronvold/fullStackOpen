787bdf8efba54b3426e9c9b191601800752b2d21
TG <thomas.groenvold@gmail.com>
2025-02-05 10:20:53 +0100

    subpart "a" Done
    a Login in frontend

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,6 +1,7 @@
 /* Imports */
 import { useState, useEffect } from "react";
 /* Components */
+import CreateBlog from "./components/CreateBlog";
 import Blog from "./components/Blog";
 import Login from "./components/Login";
 import Notification from "./components/Notification";
@@ -10,7 +11,7 @@ import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [notificationMessage, setNotificationMessage] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
@@ -45,30 +46,51 @@ const App = () => {
       setUsername("");
       setPassword("");
     } catch (exception) {
-      setErrorMessage("Wrong credentials");
+      setNotificationMessage("Wrong credentials");
       setTimeout(() => {
-        setErrorMessage(null);
+        setNotificationMessage(null);
       }, 5000);
     }
   };
 
   const handleLogout = () => {
-    window.localStorage.removeItem("loggedBlogUser");
-    setUser(null);
+      window.localStorage.removeItem("loggedBlogUser");
+      setUser(null);
   };
+
+  const handleAddNewBlog = async (newBlog) => {      
+      try {
+         const blog = await blogService.create(newBlog);
+         setBlogs(blogs => [...blogs, blog]);
+         // Set notification message then clear it after 5 seconds
+         setNotificationMessage(`A new blog ${blog.title} by ${blog.author} added`);
+         setTimeout(() => {
+         setNotificationMessage(null);
+         }, 5000);
+      } catch (ex) {
+         // Set notification message then clear it after 5 seconds
+         setNotificationMessage("Failed to add new blog");
+         setTimeout(() => {
+         setNotificationMessage(null);
+         }, 5000);
+      }
+   }
 
   return (
     <div>
-      <Notification message={errorMessage} />
+      <Notification message={notificationMessage} />
 
       {user === null ? (
         <Login
-          handleLogin={handleLogin}
-          setUsername={setUsername}
-          setPassword={setPassword}
+         handleLogin={handleLogin}
+         setUsername={setUsername}
+         setPassword={setPassword}
         />
       ) : (
-        <Blog blogs={blogs} user={user} handleLogout={handleLogout} />
+         <>
+           <Blog blogs={blogs} user={user} handleLogout={handleLogout} />
+           <CreateBlog handleAddNewBlog={handleAddNewBlog} />
+         </>
       )}
     </div>
   );
---
+++ part5/src/components/CreateBlog.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+const CreateBlog = ({ handleAddNewBlog }) => {
+
+   const [title, setTitle] = useState("");
+   const [author, setAuthor] = useState("");
+   const [url, setUrl] = useState("");
+
+   const newBlogPost = (e) => {
+      e.preventDefault();
+      handleAddNewBlog({
+         title,
+         author,
+         url,
+      });
+      setTitle("");
+      setAuthor("");
+      setUrl("");
+   }
+
+  return (
+   <div>
+      <h2>Create New</h2>
+      <form onSubmit={newBlogPost}>
+         <table>
+            <tbody>
+            <tr>
+               <td>
+                  <label htmlFor="title">Title: </label>
+               </td>
+               <td>
+                  <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  onChange={({ target }) => setTitle(target.value)}
+                  />
+               </td>
+            </tr>
+            <tr>
+               <td>
+                  <label htmlFor="author">Author: </label>
+               </td>
+               <td>
+                  <input
+                  type="text"
+                  name="author"
+                  id="author"
+                  onChange={({ target }) => setAuthor(target.value)}
+                  />
+               </td>
+            </tr>
+            <tr>
+               <td>
+                  <label htmlFor="url">URL: </label>
+               </td>
+               <td>
+                  <input
+                     type="text"
+                     name="url"
+                     id="url"
+                     onChange={({ target }) => setUrl(target.value)}
+                  />
+               </td>
+            </tr>
+            </tbody>
+         </table>
+         <button>Add</button>
+      </form>
+   </div>
+  );
+};
+
+export default CreateBlog;
